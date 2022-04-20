dc9d1ef5485d948a4363134a8134c5d6b0c0a86b
Dogan <pelin.dogan1@t-mobile.com>
2022-04-19 22:30:31 -0400

    Passed all specs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
 import './App.css';
+import React, {useState} from "react";
 
-function App() {
+const App = () => {
+    const [recipes, setRecipes] = useState([]);
+    const [isShowingForm, setIsShowingForm] = useState(false);
+    
+    const renderRecipes = (recipes) => recipes.map((recipe)=> {
+        console.log(recipes);
+        return( 
+            <li>{recipe}</li>
+        );
+    })
+ 
+    const renderForm = () => {
+        console.log('Here')
+       let currRecipes = [...recipes];
+        return (
+            
+            <form onSubmit={(event) => {
+                currRecipes.push(event.target[0]._valueTracker.getValue());
+                setRecipes(currRecipes);
+                setIsShowingForm(false);
+                event.preventDefault();
+            }}>
+                <label>
+                    Recipe Name:
+                    <input type="text" name="recipe-name"   />
+                    <br/><br/>
+                    Recipe Instruction:
+                    <input type="text" name="recipe-instructions" />
+                    <br/><br/>
+                    <input type="submit" value="Submit" />
+                </label>
+            </form>
+        );
+        
+    }
   return (
     <div>
+        <h1 className="heading">My Recipes</h1>
+        <div>
+            {
+                recipes.length === 0  ?  <h3>There are no recipes to list</h3> : <ul>{renderRecipes(recipes)}</ul> 
+            }
+        </div>
+        
+        <div>
+            {
+                isShowingForm ? <div>{renderForm()}</div> : <button onClick={ () => setIsShowingForm(true)}>Add Recipe</button>
+            }
+        </div>
       <h1 className="doNotRemoveMe">Hello world.</h1>
-      {/* ^ Do not remove this element ^ */}
+        {/* ^ Do not remove this element ^ */}
     </div>
+      
+      
   );
 }
 
